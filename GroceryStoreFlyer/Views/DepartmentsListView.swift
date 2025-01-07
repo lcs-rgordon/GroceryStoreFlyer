807e27d2d6309b3607e01e933aa1587f0c30af02
Russell Gordon <rgordon@lcs.on.ca>
2025-01-06 20:19:59 -0500

    Created a scrollable list of departments.

---
+++ GroceryStoreFlyer/Views/DepartmentsListView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  GroceryStoreFlyer
+//
+//  Created by Russell Gordon on 2025-01-06.
+//
+
+import SwiftUI
+
+struct DepartmentsListView: View {
+    
+    var body: some View {
+        
+        NavigationStack {
+            
+            List(thisWeeksFlyer.departments) { currentDepartment in
+                
+                NavigationLink {
+                    EmptyView()
+                } label: {
+                    Text(currentDepartment.name)
+                        .font(.title2)
+                }
+                
+            }
+            .navigationTitle("Weekly Flyer")
+            
+        }
+        
+    }
+}
+
+#Preview {
+    DepartmentsListView()
+}
